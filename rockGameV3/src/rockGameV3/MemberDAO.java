1d2f28bafde444dfdcc951ba3689b8e0db588c1b
moonjikwang <moonjikwang@218.38.137.28>
2023-01-06 00:55:35 +0900

    비밀번호 변경 메서드 추가, 아이디,서버분리 메서드 추가

--- rockGameV3/src/rockGameV3/MemberDAO.java
+++ rockGameV3/src/rockGameV3/MemberDAO.java
@@ -6,27 +6,32 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 
 public class MemberDAO {
-
+//---------------------필드선언 및 초기화 --------------------
 	private static MemberDAO instance = new MemberDAO();
 	private MemberDTO member;
 	private static String rootFolder = "Members/";
 	private static File folder = new File(rootFolder);
+	private static Scanner sc = new Scanner(System.in);
 	FileWriter fw;
 	BufferedWriter bw;
 	FileReader fr;
 	BufferedReader br;
-
+//---------------------필드선언 및 초기화 끝 --------------------
+	
+	//-----------------싱글톤 작업 --------------------
 	private MemberDAO() {
 	}
 	
 	public static MemberDAO getInstance() {
 		return instance;
 	}
+	//-----------------싱글톤 작업끝  --------------------
 
-	// 회원가입메서드
+	//------------------------ 회원가입메서드------------------------
 	public int registerId(MemberDTO member) {
 		int result = 0;// 결과값 flag.. 모두 OK 1, 예외 0
 
@@ -49,25 +54,23 @@ public class MemberDAO {
 					bw.write("Count:" + member.getCount() + "\n");
 					bw.close();
 					result = 1;
+					bw.close();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
-					System.out.println("사용자 생성 예외 발생함..");
 					System.out.println(e.getMessage());
 				}
-
 			}
 		}
-
 		return result;
 	}
+	//------------------------ 회원가입메서드 끝 ------------------------
 
-	// 로그인 메서드
+	//------------------------로그인 메서드------------------------
 	public int logIn(MemberDTO member) {
 		System.out.print("멤버파일 ->");
 		int result = -1; // ID 없음, 0 Pass 틀림, 1 OK
 		File[] fileList = folder.listFiles();
-		String email = member.getEmail();
-		String id = email.substring(0,email.indexOf('@')) + ".dat";
+		String id = divideId(member.getEmail());
 		String password = null;
 		File thePlayer = null;
 		for (int i = 0; i < fileList.length; i++) {
@@ -82,19 +85,80 @@ public class MemberDAO {
 		try {
 			fr = new FileReader(thePlayer.getAbsolutePath());
 			br = new BufferedReader(fr);
-			String keySerch = null;
-			while ((keySerch = br.readLine()) != null) {
-				if (keySerch.startsWith("Password")) {
-					password = keySerch.substring(keySerch.indexOf(":") + 1, keySerch.length());
+			String keySearch = null;
+			while ((keySearch = br.readLine()) != null) {
+				if (keySearch.startsWith("Password")) {
+					password = keySearch.substring(keySearch.indexOf(":") + 1, keySearch.length());
 					if (password.equals(member.getPassword())) {
 						result = 1;
 					}
 				}
 			}
+			br.close();
 		} catch (Exception e) {
 			// TODO: handle exception
-		}}
+		}
+		}
 		return result;
 	}
+	//------------------------로그인 메서드 끝------------------------
+	
+	//--------------------------비밀번호 변경 메서드--------------------------
+	public int changePw(String nowPassword) {
+		int result = -1; // -1 기존패스워드 틀림 .
+		try {
+		String id = divideId(Login.member.getEmail());
+		File file = new File(rootFolder,id);
+		br = new BufferedReader(new FileReader(file));
+		String keySearch = null;
+		while ((keySearch = br.readLine()) != null) {
+			if (keySearch.startsWith("Password")) {
+				String password = keySearch.substring(keySearch.indexOf(":") + 1, keySearch.length());
+				if (password.equals(nowPassword)) {
+					result = 0; // 0 기존 패스워드 일치
+				}
+			}
+		}
+		br.close();
+		
+		}catch (Exception e) {
+		}
+		return result;
+	}
+	public int newPasswoard(String newPassword) {
+		int result = -1;
+		String id = divideId(Login.member.getEmail());
+		File file = new File(rootFolder,id);
+		try {
+		br = new BufferedReader(new FileReader(file));
+		String newFile = ""; //새 계정정보 DB값을 담을 문자열
+		String temp;
+		while((temp = br.readLine()) != null) {
+			if(temp.startsWith("Password")) {
+				newFile += (temp.substring(0, temp.indexOf(":")+1) + newPassword + "\n");
+				result = 0;
+				continue;
+			}
+			newFile += (temp + "\n");
+		}
+		
+		fw = new FileWriter(file);
+		fw.write(newFile);
+		
+		fw.close();
+		br.close();
+		
+		}catch (Exception e) {
+		}
+		return result;
+	}
+	//--------------------------비밀번호 변경 메서드끝 --------------------------
+	
+	//-----------------------아이디분리 메서드-------------------------------
+	public String divideId(String email) {
+		String id = email.substring(0,email.indexOf('@')) + ".dat";
+		return id;
+	}
+	//-----------------------아이디분리 메서드 끝------------------------------
 
 }
