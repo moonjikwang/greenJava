d86e3202f43a6fb9bf46b40f3d4c78c5f9574ed8
cocp-061g <cocp-061g@cocp-061>
2023-01-06 17:50:54 +0900

    스위치문, 메소드명까지 작성 - PlayerInfo에 메소드 작성해서 인스턴스로 불러올지?

--- rockGameV3/src/rockGameV3/OtherMenu.java
+++ rockGameV3/src/rockGameV3/OtherMenu.java
@@ -1,15 +1,56 @@
 package rockGameV3;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author 이유현
  *
  */
 /*
- * Menu클래스에서 기타메뉴보기를 클릭하여 호출된 클래스입니다.
- * 아래 메뉴를 출력하고 입력값을 받아 해당 메서드를 호출합니다.
- * 1.총 사용자수 2.1위플레이어 3.승률꼴지부터보기 4.승률1위부터보기
- * id의 끝 3자리는 ***로 표시해야합니다.(승률도 표시)
+ * Menu클래스에서 기타메뉴보기를 클릭하여 호출된 클래스입니다. 아래 메뉴를 출력하고 입력값을 받아 해당 메서드를 호출합니다. 1.총
+ * 사용자수 2.1위플레이어 3.승률꼴지부터보기 4.승률1위부터보기 id의 끝 3자리는 ***로 표시해야합니다.(승률도 표시)
  */
 public class OtherMenu {
+	static Scanner sc = new Scanner(System.in);
+	PlayerInfo info = new PlayerInfo();
 
+	public OtherMenu() {
+		System.out.println("<기타메뉴>");
+		System.out.println("1.총 사용자수 2.1위플레이어 3.승률꼴지부터보기 4.승률1위부터보기");
+		System.out.print(">");
+		int select = sc.nextInt();
+		switch (select) {
+		case 1:
+			totalPlayer();
+			break;
+		case 2:
+			topPlayer();
+			break;
+		case 3:
+			ascRank();
+			break;
+		case 4:
+			descRank();
+			break;
+		default:
+			break;
+		}
+	}
+	private void descRank() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void ascRank() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void topPlayer() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void totalPlayer() {
+		System.out.println("총플레이어 테스트");
+		
+	}
 }
