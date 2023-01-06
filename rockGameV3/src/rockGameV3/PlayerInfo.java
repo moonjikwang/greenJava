93861497d2868c26161c29bd5eacda65e7d9bead
cocp-061g <cocp-061g@cocp-061>
2023-01-06 17:52:09 +0900

    폴더내 파일을 한번에 가져오는 방법 연구중-일단 테스트용폴더와 파일로 연구중

--- rockGameV3/src/rockGameV3/PlayerInfo.java
+++ rockGameV3/src/rockGameV3/PlayerInfo.java
@@ -4,6 +4,43 @@ package rockGameV3;
  * @author 이유현
  *
  */
+//플레이어관련 메서드2개필요 . 총플레이어수 메서드,랭킹1위 메서드
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Vector;
+
 public class PlayerInfo {
-// 플레이어관련 메서드2개필요 . 총플레이어수 메서드,랭킹1위 메서드
+
+	//폴더 내 파일을 읽어오는 메서드
+//	public static void findAllFilesInFolder() {
+//		File folder = new File("users");
+//		for (File file : folder.listFiles()) {
+//			if (!file.isDirectory()) {
+//				System.out.println(file.getName());
+//			} else {
+//				findAllFilesInFolder();
+//			}
+//		}
+//	}
+	
+public static void FileInput() {
+	File file = new File("users/user1.dat");
+	
+	try {
+		InputStream fis = new FileInputStream(file);
+		
+		
+		
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	
+	
+}
 }
