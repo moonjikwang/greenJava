6cca157810fc352d4e124fe7e20e6ca704262d65
moonjikwang <moonjikwang@218.38.137.28>
2023-01-09 00:54:54 +0900

    가이드추가

--- rockGameV3/src/rockGameV3/Register.java
+++ rockGameV3/src/rockGameV3/Register.java
@@ -6,7 +6,12 @@ package rockGameV3;
  */
 import java.util.Scanner;
 
-
+/*
+ * 이메일을 입력받고 입력받은 값을 파라미터로 받는 Validate() 메서드를 호출합니다.
+ * 검증에 이상이없으면, 패스워드를 입력받고, 이메일과 패스워드를 파라미터로 받는 MemberDTO 인스턴스 생성합니다.
+ * 이메일과 패스워드정보가 담긴 MemberDTO 객체를 DB에 저장하기위해 해당 객체를 파라미터로 받는 MemberDAO.getInstance().registerId()메서드를 호출합니다.
+ * 해당 메서드는 회원가입이 정상적으로 이루어지면 정수 1을 리턴하고, 오류가 발생하면 0을 리턴합니다.
+ */
 
 
 public class Register {
