84b7700f950285d19883827a49c88456e7b0dfe6
22-ncore-020g <22-ncore-020g@22-ncore-020>
2023-01-09 17:17:53 +0900

    불필요내용 삭제

--- rockGameV3/src/rockGameV3/EmailCheck.java
+++ rockGameV3/src/rockGameV3/EmailCheck.java
@@ -13,18 +13,19 @@ public class EmailCheck {
 	public void validate(String email) {
 		if(email.isEmpty() || email.indexOf('@') == -1) {
 			System.out.println("빈 문자열이거나 @가 없습니다.");
+			new Register();
 		}
 		String id = email.substring(0,email.indexOf('@'));
 		//String server = email.substring(email.indexOf('@'),email.length());
 		if(id.length() >12 || id.length() < 8) {
 			System.out.println("아이디는 8~ 12자 사이여야 합니다.");
-			System.exit(0);
+			new Register();
 		}else if(id.charAt(0) < 65 || id.charAt(0) > 90) {
 			System.out.println("아이디의 첫자는 영어 대문자여야 합니다.");
-			System.exit(0);
+			new Register();
 		}else if(!id.matches(".*[0-9].*")) {
 			System.out.println("아이디에는 숫자가 하나이상 들어가야합니다.");
-			System.exit(0);
+			new Register();
 		}else {
 			System.out.println("이메일 검증완료");
 		}
