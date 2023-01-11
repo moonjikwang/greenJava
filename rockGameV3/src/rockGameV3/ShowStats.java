74e5ee52771dda062b5c85156bb62add47220ad8
18-cocp-008g <18-cocp-008g@18-cocp-008>
2023-01-11 09:18:46 +0900

    Merge branch 'main' of https://github.com/moonjikwang/greenJava.git into main

--- rockGameV3/src/rockGameV3/ShowStats.java
+++ rockGameV3/src/rockGameV3/ShowStats.java
@@ -12,7 +12,7 @@ import java.util.TreeMap;
  *  이 클래스는 로그인성공 -> 게임메뉴에서 전적보기 클릭하여 호출된클래스입니다.
  * 해당사용자의 누적 전적을 출력하고 게임을 종료합니다.
  */
-public class ShowStats {
+public class ShowStats {//
 
 	static Scanner sc = new Scanner(System.in);
 	public ShowStats() {
