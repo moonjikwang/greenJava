d166779b6c11e20f3bb9921d4e958ea753d1dce6
18-cocp-014g <18-cocp-014g@18-cocp-014>
2023-01-12 14:48:47 +0900

    회원가입후 로그인/메뉴 단계 수정

--- rockGameV3/src/rockGameV3/Register.java
+++ rockGameV3/src/rockGameV3/Register.java
@@ -35,8 +35,17 @@ public class Register {
 		MemberDTO member = new MemberDTO(email,password);
 		int newMember = MemberDAO.getInstance().registerId(member);
 		if (newMember == 1) {
-			System.out.println("회원가입이 완료되었습니다.");
-				new Menu();
+			System.out.println("회원가입이 완료되었습니다!");
+			System.out.println("로그인하시겠습니까?");
+			System.out.println("로그인하시려면 Y, 메뉴로 돌아가려면 N을 입력하세요.");
+			while (true) {
+				String ynCheck = sc.next();
+				if(ynCheck.equals("Y")||ynCheck.equals("y")) {
+					new Login();
+				}else if (ynCheck.equals("N")||ynCheck.equals("n")) {
+					new Menu();
+				}
+			}
 			} else {
 				// Yes 가 아닌 다른 경우는 일단 종료 시키는 로직으로 했으나 여러분이 다른걸로 바꿔보세요~
 				System.out.println("회원가입실패. 종료합니다.");
