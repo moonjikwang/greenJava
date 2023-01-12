5fd1324c191c3ec40556916967055a917c9b32d1
18-cocp-014g <18-cocp-014g@18-cocp-014>
2023-01-12 09:45:27 +0900

    Merge branch 'main' of https://github.com/moonjikwang/greenJava.git into main

--- rockGameV3/src/rockGameV3/Login.java
+++ rockGameV3/src/rockGameV3/Login.java
@@ -2,24 +2,19 @@ package rockGameV3;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Scanner;
+
 /**
  * 
  * @author 백승연
  *
  */
 /*
- * 이메일을 입력받아서, 해당 id.data 파일의 존재여부 확인하여
- * 존재하면 패스워드받기. 패스워드 일치시 -> id님 로그인을 환영합니다.
- * 불일치시 -> 암호가 틀립니다. (3회 기회 -> 관리자에게 연락바랍니다.프로그램종료)
- * 존재하지 않으면 -> 회원가입해주세요 
+ * 이메일을 입력받아서, 해당 id.data 파일의 존재여부 확인하여 존재하면 패스워드받기. 패스워드 일치시 -> id님 로그인을 환영합니다.
+ * 불일치시 -> 암호가 틀립니다. (3회 기회 -> 관리자에게 연락바랍니다.프로그램종료) 존재하지 않으면 -> 회원가입해주세요
  *
- * 
- * <Login 구현>
- * 로그인성공
- * -> 마지막 로그인 날짜 출력 : String(date)
- * -> GameMenu클래스 호출(인스턴스생성)
+ * <Login 구현> 로그인성공 -> 마지막 로그인 날짜 출력 : String(date) -> GameMenu클래스 호출(인스턴스생성)
  */
-import java.util.Scanner;
 
 //Login 검증 후 로그인기록 추가 & GameMenu 호출
 //Login 클래스 가이드
@@ -31,12 +26,10 @@ import java.util.Scanner;
 public class Login {
 	static Scanner sc = new Scanner(System.in);
 	protected static MemberDTO member = null;
-
-	// 필드정의
 	String email;
 	String password;
 	String date;
-	int temp = 0;//
+	int temp = 0;
 
 	public Login() {
 		System.out.print("ID : ");
@@ -52,13 +45,14 @@ public class Login {
 	public void logInVal(int logInVal) {
 		switch (logInVal) {
 		case -1:
-			// 아이디 없으면 -> 회원가입해주세요, Register() 클래스호출
-				System.out.println("해당 아이디는 없습니다. 회원가입을 진행합니다.");
+			// 아이디 없는경우 -> 회원가입해주세요, Register() 클래스호출
+			if (email == null) {
+				System.out.println("해당 아이디는 없습니다.");
 				new Register();
+			}
 			break;
 		case 0:
-			temp++;
-			// 비번틀림, 3회기회 -> 관리자에게 연락바랍니다(프로그램종료)
+			// 비번틀린경우 -> 3회 기회, 관리자에게 연락바랍니다(프로그램종료)
 			if (temp < 3) {
 				System.out.println("비밀번호가 틀렸습니다." + temp + "회 시도중");
 				reInputPw();
@@ -68,9 +62,10 @@ public class Login {
 			}
 			break;
 		case 1:
-			// 로그인 성공
+			// 로그인 성공한 경우
 			// 마지막 로그인 날짜 출력, GameMenu()클래스 호출(인스턴스생성)
 			System.out.println("로그인에 성공했습니다. " + email + "님 반갑습니다.");
+			System.out.println(member.getLastLogIn());
 			new GameMenu();
 			break;
 		default:
@@ -78,7 +73,7 @@ public class Login {
 		}
 	}
 
-	public void reInputPw() {
+	public void reInputPw() {//상속,,?
 		System.out.print("PW : ");
 		password = sc.next();
 		date = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss").format(Calendar.getInstance().getTime());
